080d559724e676bddf62b910edd0a22dc0be3042
Donabel Liotta <belramacho990@gmail.com>
2025-02-09 17:08:49 -0500

    added footer links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       Weather App
+   
+    <footer>
+      This project was coded by 👩🏻‍💻 <a href="https://github.com/belram990">Donabel Liotta</a>{" "}
+      and is open-sourced on <a href="https://github.com/belram990/weather-app-react">GitHub</a>
+       {" "}hosted by <a href="dbweather-app.netlify.app">Netlify</a>.
+    </footer>
     </div>
   );
 }
