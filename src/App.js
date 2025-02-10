5a219c1f1e5929fdba6d006194afa298cd2eff50
Donabel Liotta <belramacho990@gmail.com>
2025-02-10 11:15:03 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <div className = "container">
+      <h1>Weather App</h1>
    
     <footer>
       This project was coded by 👩🏻‍💻 <a href="https://github.com/belram990">Donabel Liotta</a>{" "}
       and is open-sourced on <a href="https://github.com/belram990/weather-app-react">GitHub</a>
        {" "}hosted by <a href="dbweather-app.netlify.app">Netlify</a>.
     </footer>
+    </div>
     </div>
   );
 }
