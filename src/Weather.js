b2bdd1233a80103190f30ae9d8a596b3f714879c
Donabel Liotta <belramacho990@gmail.com>
2025-02-10 14:06:58 -0500

    added css

--- src/Weather.js
+++ src/Weather.js
@@ -8,24 +8,26 @@ export default function Weather (){
             <form>
                 <div className="row">
                     <div className="col-9">
-                <input type="serch" placeholder="Enter a city..." className="form-contol"/>     
+                <input type="serch" placeholder="Enter a city..." className="form-contol w-100" autoFocus="on" />     
             </div>
             <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary"/>
+                <input type="submit" value="Search" className="btn btn-primary w-100"/>
                 </div>
                 </div>
             </form>
 <h1>New York</h1>
 <ul>
-    <li>Monday 11:00</li>s
+    <li>Monday 11:00 AM</li>
     <li>Sunny</li>
 </ul>
-<div className="row">
-    <div className="col-6"></div>
-    ☀️
-    33°F
+<div className="row ">
+    <div className="col-6 ">
+    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny"/>
+    <span className="value">33</span>
+    <span className="unit">°F</span>
+    
 </div>
-<div className="col-6">
+<div className="col-6 mt-3">
     <ul>
         <li>
 Percipitation: 0%
@@ -38,7 +40,7 @@ Percipitation: 0%
         </li>
     </ul>
 </div>
-
+</div>
         </div>
     );
 }
