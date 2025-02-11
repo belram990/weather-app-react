5d5d393ec7ceed3c0eb18338e99da7567f806965
Donabel Liotta <belramacho990@gmail.com>
2025-02-11 10:19:21 -0500

    added animated icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -4,11 +4,29 @@ import ReactAnimatedWeather from 'react-animated-weather';
 import "./Weather.css";
 
 export default function WeatherIcon (props){
-   
+   console.log(props.code);
+   let codeMapping = {
+"clear-sky-day": "CLEAR_DAY",
+"clear-sky-night": "CLEAR_NIGHT",
+"few-clouds-day": "PARTLY_CLOUDY_DAY",
+"few-clouds-night": "PARTLY_CLOUDY_NIGHT",
+"scattered-clouds-day": "PARTLY_CLOUDY_DAY",
+"scattered-clouds-night": "PARTLY_CLOUDY_NIGHT",
+"broken-clouds-day": "CLOUDY",
+"broken-clouds-night": "CLOUDY",
+"shower-rain-day": "RAIN",
+"shower-rain-night": "RAIN",
+"thunderstorm-day": "RAIN",
+"thunderstrom-night": "RAIN",
+"snow-day": "SNOW",
+"snow-night": "SNOW",
+"mist-day": "FOG",
+"mist-night": "FOG",
+   };
 
     return(
         <ReactAnimatedWeather
-    icon="CLEAR_DAY"
+    icon={codeMapping[props.code]}
     color="#facf5a"
     size="512"
     animate="true"
