dc2cf8546ef236caed55782f24e815aca83f36bb
Donabel Liotta <belramacho990@gmail.com>
2025-02-13 21:01:52 -0500

    updated netlify link

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <footer>
       This project was coded by 👩🏻‍💻 <a href="https://github.com/belram990">Donabel Liotta</a>{" "}
       and is open-sourced on <a href="https://github.com/belram990/weather-app-react">GitHub</a>
-       {" "}hosted by <a href="dbweather-app.netlify.app">Netlify</a>.
+       {" "}hosted by <a href="dliotta-weather-app.netlify.app">Netlify</a>.
     </footer>
     </div>
     </div>
