4bd8e38540e2d11d30b83d568928e5a4e0de6083
Donabel Liotta <belramacho990@gmail.com>
2025-02-13 20:36:32 -0500

    change button color

--- src/Weather.js
+++ src/Weather.js
@@ -55,7 +55,7 @@ if (weatherData.ready){
                 />     
             </div>
             <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary w-100"/>
+                <input type="submit" value="Search" className="btn w-100"/>
                 </div>
                 </div>
             </form>
